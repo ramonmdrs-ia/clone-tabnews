de2848a6f9ccf94b4af57404357bc926d848da82
Ramon <ramonmdrs.ia@gmail.com>
2026-04-22 15:19:16 +0000

    adiciona paina inicial

---
+++ pages/index.js
@@ -0,0 +1,5 @@
+function Home() {
+    return <h1>Você pode ainda não ter chegado lá, mas o caminho que percorreu até aqui revela o quanto você é poderosa e capaz. Continue.</h1>
+}
+
+export default Home;
